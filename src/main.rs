4253b46f6c1fe71fe4de47bf8f1358050fc9ffb8
Will Cygan <wcygan.io@gmail.com>
2023-03-16 00:30:13 -0500

    spin up the UDP listner & responders

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use futures::future::join_all;
+use std::sync::Arc;
+use tokio::net::UdpSocket;
+use tub::Pool;
+
+static DEFAULT_PORT: usize = 6565;
+
+type UdpPool = Arc<Pool<UdpSocket>>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let _listener = UdpSocket::bind(format!("0.0.0.0:{}", DEFAULT_PORT)).await?;
+    let _pool = create_pool(8).await?;
+    Ok(())
+}
+
+async fn create_pool(size: usize) -> Result<UdpPool> {
+    let futures = (0..size)
+        .map(|_| UdpSocket::bind(format!("0.0.0.0:0")))
+        .collect::<Vec<_>>();
+
+    let results = join_all(futures).await;
+
+    let sockets = results
+        .into_iter()
+        .map(|socket| socket.map_err(|e| e.into()))
+        .collect::<Result<Vec<_>>>()?;
+
+    Ok(Arc::new(Pool::from_vec(sockets)))
 }
