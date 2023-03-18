d68edf8a1d03dc7a1052475c743ef96144af7372
Will Cygan <wcygan.io@gmail.com>
2023-03-18 17:23:40 -0500

    udp server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,34 +1,37 @@
 use anyhow::Result;
-use futures::future::join_all;
 use std::sync::Arc;
-use tokio::net::UdpSocket;
+use tokio::net::{ToSocketAddrs, UdpSocket};
 use tub::Pool;
-
-static DEFAULT_PORT: usize = 6565;
-
-type UdpPool = Arc<Pool<UdpSocket>>;
-
-mod client;
-mod server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let _listener = UdpSocket::bind(format!("0.0.0.0:{}", DEFAULT_PORT)).await?;
-    let _pool = create_pool(8).await?;
-    Ok(())
+    let (listener, pool) = get_sockets().await?;
+
+    loop {
+        let mut buf = [0; 512];
+        let (size, src) = listener.recv_from(&mut buf).await?;
+        let pool = pool.clone();
+        tokio::spawn(async move {
+            let writer = pool.acquire().await;
+            writer.send_to(buf.as_slice(), src).await?;
+            Ok::<(), anyhow::Error>(())
+        });
+    }
 }
 
-async fn create_pool(size: usize) -> Result<UdpPool> {
-    let futures = (0..size)
-        .map(|_| UdpSocket::bind(format!("0.0.0.0:0")))
-        .collect::<Vec<_>>();
+async fn get_sockets() -> Result<(UdpSocket, Arc<Pool<UdpSocket>>)> {
+    let mut writers: Vec<UdpSocket> = vec![];
+    for _ in 0..10 {
+        writers.push(bind_any().await?)
+    }
 
-    let results = join_all(futures).await;
+    let listener = bind_any().await?;
+    let addr = listener.local_addr()?;
+    println!("Server started on {}", addr);
 
-    let sockets = results
-        .into_iter()
-        .map(|socket| socket.map_err(|e| e.into()))
-        .collect::<Result<Vec<_>>>()?;
+    Ok((listener, Arc::new(writers.into())))
+}
 
-    Ok(Arc::new(Pool::from_vec(sockets)))
+async fn bind_any() -> Result<UdpSocket> {
+    Ok(UdpSocket::bind("0.0.0.0:0").await?)
 }
