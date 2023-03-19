edf8fdc3245cf3dde1e8dc5cfbc8b67a55c0642b
Will Cygan <wcygan.io@gmail.com>
2023-03-19 09:08:16 -0700

    clap for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,15 @@
 use anyhow::Result;
-use common::{FromServer, ToServer, UdpConnection, DEFAULT_ADDRESS};
+use clap::Parser;
+use common::{FromServer, ToServer, UdpConnection};
 use std::sync::Arc;
 use tokio::net::UdpSocket;
 use tub::Pool;
 
+mod args;
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let (mut conn, pool) = get_sockets(DEFAULT_ADDRESS.into()).await?;
+    let (mut conn, pool) = init().await?;
 
     loop {
         let (msg, src) = conn.read::<ToServer>().await?;
@@ -28,20 +31,21 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn get_sockets(addr: Option<&str>) -> Result<(UdpConnection, Arc<Pool<UdpConnection>>)> {
+async fn init() -> Result<(UdpConnection, Arc<Pool<UdpConnection>>)> {
+    let args = args::Args::parse();
     let mut writers: Vec<UdpConnection> = vec![];
     for _ in 0..10 {
         writers.push(UdpConnection::new(get_socket(None).await?))
     }
 
-    let listener = get_socket(addr).await?;
+    let listener = get_socket(Some(args.address)).await?;
     let addr = listener.local_addr()?;
     println!("Server started on {}", addr);
 
     Ok((UdpConnection::new(listener), Arc::new(writers.into())))
 }
 
-async fn get_socket(addr: Option<&str>) -> Result<UdpSocket> {
+async fn get_socket(addr: Option<String>) -> Result<UdpSocket> {
     let socket = match addr {
         None => UdpSocket::bind("0.0.0.0:0").await?,
         Some(a) => UdpSocket::bind(a).await?,
