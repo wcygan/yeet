1a50563a518e605617cc2673266f177fcfa062cf
Will Cygan <wcygan.io@gmail.com>
2023-03-20 11:31:34 -0700

    client rebuild 2: udp socket binding

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,22 @@
 use crate::keyboard_input::recv_from_stdin;
+use anyhow::Result;
+use clap::Parser;
+use common::Socket;
 use lib_wc::sync::{ShutdownController, ShutdownListener};
 use std::io::{stdin, BufRead, BufReader};
+use std::net::SocketAddr;
 use std::time::Duration;
+use tokio::net::UdpSocket;
 use tokio::select;
 use tokio::sync::mpsc;
 
+mod args;
 mod keyboard_input;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    let (mut conn, addr) = init().await?;
+    let server_addr: SocketAddr = addr.parse()?;
     let shutdown = ShutdownController::new();
     let listener = shutdown.subscribe();
 
@@ -19,14 +27,16 @@ async fn main() {
         },
         _ = tokio::spawn(async { process(listener).await }) => {}
     }
+
+    Ok(())
 }
 
-async fn process(mut sd: ShutdownListener) {
+async fn process(mut sd: ShutdownListener) -> Result<()> {
     let mut chan = recv_from_stdin();
     while !sd.is_shutdown() {
         select! {
             _ = sd.recv() => {
-                return;
+                break
             },
             line = chan.recv() => {
                 if let Some(s) = line {
@@ -35,4 +45,12 @@ async fn process(mut sd: ShutdownListener) {
             }
         }
     }
+
+    Ok(())
+}
+
+async fn init() -> Result<(Socket, String)> {
+    let args = args::Args::parse();
+    let socket = UdpSocket::bind("0.0.0.0:0").await?;
+    Ok((Socket::new(socket), args.address))
 }
