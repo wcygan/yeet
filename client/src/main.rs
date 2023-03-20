1568943e7cb8b54fc51e820226e36d66b171f603
Will Cygan <wcygan.io@gmail.com>
2023-03-20 11:26:37 -0700

    wait for shutdown

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,7 @@
 use crate::keyboard_input::recv_from_stdin;
 use lib_wc::sync::{ShutdownController, ShutdownListener};
 use std::io::{stdin, BufRead, BufReader};
+use std::time::Duration;
 use tokio::select;
 use tokio::sync::mpsc;
 
@@ -9,26 +10,28 @@ mod keyboard_input;
 #[tokio::main]
 async fn main() {
     let shutdown = ShutdownController::new();
+    let listener = shutdown.subscribe();
+
     select! {
         _ = tokio::signal::ctrl_c() => {
             shutdown.shutdown().await;
+            println!("Done!");
         },
-        _ = process(shutdown.subscribe()) => {}
+        _ = tokio::spawn(async { process(listener).await }) => {}
     }
 }
 
-async fn process(mut listener: ShutdownListener) {
+async fn process(mut sd: ShutdownListener) {
     let mut chan = recv_from_stdin();
-    while !listener.is_shutdown() {
+    while !sd.is_shutdown() {
         select! {
-            _ = listener.recv() => {
-                return
+            _ = sd.recv() => {
+                return;
             },
             line = chan.recv() => {
-                    match line {
-                        Some(s) => println!("{s}"),
-                        None => {}
-                    }
+                if let Some(s) = line {
+                    println!("{s}")
+                }
             }
         }
     }
