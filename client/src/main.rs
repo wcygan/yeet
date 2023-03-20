0058abc2d748ea7942f50b58416840d910801a33
Will Cygan <wcygan.io@gmail.com>
2023-03-20 10:27:09 -0700

    setup main loop with shutdown

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,5 @@
 use crate::keyboard_input::recv_from_stdin;
+use lib_wc::sync::{ShutdownController, ShutdownListener};
 use std::io::{stdin, BufRead, BufReader};
 use tokio::select;
 use tokio::sync::mpsc;
@@ -7,19 +8,27 @@ mod keyboard_input;
 
 #[tokio::main]
 async fn main() {
-    let mut chan = recv_from_stdin();
+    let shutdown = ShutdownController::new();
+    select! {
+        _ = tokio::signal::ctrl_c() => {
+            shutdown.shutdown().await;
+        },
+        _ = process(shutdown.subscribe()) => {}
+    }
+}
 
-    loop {
+async fn process(mut listener: ShutdownListener) {
+    let mut chan = recv_from_stdin();
+    while !listener.is_shutdown() {
         select! {
-            _ = tokio::signal::ctrl_c() => {
-                println!("Done");
-                return;
-            }
+            _ = listener.recv() => {
+                return
+            },
             line = chan.recv() => {
-                match line {
-                    Some(s) => println!("{s}"),
-                    None => {}
-                }
+                    match line {
+                        Some(s) => println!("{s}"),
+                        None => {}
+                    }
             }
         }
     }
